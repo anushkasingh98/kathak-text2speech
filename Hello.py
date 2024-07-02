f361498bdf146eaebc3188a0f97128230a0d2b97
Anushka Singh <31279432+anushkasingh98@users.noreply.github.com>
2024-07-02 01:03:42 -0400

    Create Hello.py

---
+++ Hello.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("Kathak Text2Speech")
+
+st.write("This is a simple web app that converts text to speech for Kathak bol's.")
+
+st.info("This app is currently under development! We will be launching very soon!")
